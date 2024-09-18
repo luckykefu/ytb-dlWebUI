405fa01936c8a48864a6538f0d092ba632e3babc
Luckykefu <3124568493@qq.com>
2024-09-18 20:03:00 +0800

    Auto-commit at 2024-09-18 20:01:58

--- src/log.py
+++ src/log.py
@@ -1,56 +1,54 @@
 # log.py
 # --coding:utf-8--
-# Time:2024-09-17 08:14:40
-# Author:Luckykefu
+# Time:2024-09-18 19:48:05
+# Author:luckykefu
 # Email:3124568493@qq.com
-# Description:
+# Description: Provides colored logging functionality
 
 import logging
 from colorama import Fore, Style, init
+from functools import lru_cache
 
 # Initialize colorama
 init(autoreset=True)
 
 # Define colors for different levels
 LEVEL_COLORS = {
-    logging.DEBUG: Fore.BLUE,      # Debug messages in blue
-    logging.INFO: Fore.GREEN,       # Info messages in green
-    logging.WARNING: Fore.YELLOW,    # Warning messages in yellow
-    logging.ERROR: Fore.RED,        # Error messages in red
-    logging.CRITICAL: Fore.MAGENTA,  # Critical messages in magenta
+    logging.DEBUG: Fore.BLUE,
+    logging.INFO: Fore.GREEN,
+    logging.WARNING: Fore.YELLOW,
+    logging.ERROR: Fore.RED,
+    logging.CRITICAL: Fore.MAGENTA,
 }
+
 
 class ColoredFormatter(logging.Formatter):
     def format(self, record):
-        # Get the color for the record
         color = LEVEL_COLORS.get(record.levelno, Style.RESET_ALL)
-        # Set the log format
         message = super().format(record)
         return f"{color}{message}{Style.RESET_ALL}"
 
+
+@lru_cache(maxsize=None)
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-
-    # Use the custom ColoredFormatter
-    formatter = ColoredFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
-
-    # Clear duplicate handlers
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
-    logger.addHandler(console_handler)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        formatter = ColoredFormatter(
+            "%(asctime)s - %(levelname)s - %(pathname)s:%(lineno)d - %(message)s"
+        )
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
     return logger
+
 
 if __name__ == "__main__":
     logger = get_logger("MyLogger")
-
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
     logger.error("This is an error message.")
     logger.critical("This is a critical message.")
+    
