4638ad1b97e3d03b6e14e7769c0faf6a58110393
Luckykefu <3124568493@qq.com>
2024-09-19 05:32:19 +0800

    Auto-commit at 2024-09-19 05:31:33

--- src/log.py
+++ src/log.py
@@ -1,9 +1,9 @@
 # log.py
 # --coding:utf-8--
-# Time:2024-09-18 19:48:05
+# Time:2024-09-19 05:25:29
 # Author:luckykefu
 # Email:3124568493@qq.com
-# Description: Provides colored logging functionality
+# Description: Provides colored logging functionality with enhanced features
 
 import logging
 from colorama import Fore, Style, init
@@ -14,12 +14,12 @@ init(autoreset=True)
 
 # Define colors for different levels
 LEVEL_COLORS = {
-    logging.DEBUG: Fore.BLUE,
+    logging.DEBUG: Fore.CYAN,
     logging.INFO: Fore.GREEN,
     logging.WARNING: Fore.YELLOW,
     logging.ERROR: Fore.RED,
-    logging.CRITICAL: Fore.MAGENTA,
-}
+    logging.CRITICAL: Fore.MAGENTA + Style.BRIGHT,
+    }
 
 
 class ColoredFormatter(logging.Formatter):
@@ -30,22 +30,36 @@ class ColoredFormatter(logging.Formatter):
 
 
 @lru_cache(maxsize=None)
-def get_logger(name):
+def get_logger(name, log_file=None):
     logger = logging.getLogger(name)
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
         formatter = ColoredFormatter(
-            "%(asctime)s - %(levelname)s - %(pathname)s:%(lineno)d - %(message)s"
+            "%(asctime)s - %(name)s - %(levelname)s - %(pathname)s:%(lineno)d - %(message)s"
         )
+
+        # Console handler
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
+
+        # File handler (if log_file is provided)
+        if log_file:
+            file_handler = logging.FileHandler(log_file, encoding="utf-8")
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s - %(levelname)s - %(pathname)s:%(lineno)d - %(message)s"
+                )
+            )
+            logger.addHandler(file_handler)
+
     return logger
 
 
 if __name__ == "__main__":
-    logger = get_logger("MyLogger")
+    logger = get_logger(__file__)
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
